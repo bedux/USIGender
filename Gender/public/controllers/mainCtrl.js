8798d3d748611588ae17d91af441ddd2fb61d9b8
Marco Ravazzini <marcoravazzini@marcos-MBP.homenet.telecomitalia.it>
2015-07-08 00:31:34 +0200

    add possibility to change font size
    simply adding the class: “fontResizable” to an element it became
    modifiable by the user

--- Gender/public/controllers/mainCtrl.js
+++ Gender/public/controllers/mainCtrl.js
@@ -1,5 +1,21 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('mainCtrl', ['$scope', function($scope) {
-  $scope.categories = ['cane', 'gatto', 'gallina', 'topo','cinghiale','gorilla'];
+	$scope.categories = ['cane', 'gatto', 'gallina', 'topo','cinghiale','gorilla'];
+
+	$scope.changeFontSize = function(step){
+		step = parseInt(step,10);
+		var el = document.getElementsByClassName("fontResizable");
+		console.log(el.length);
+        
+        for(a=0 ; a < el.length; a++){
+            var curFont = parseInt(el[a].style.fontSize,10)||14;
+            el[a].style.fontSize = (curFont+step) + 'px';
+        
+        }
+        
+//        var curFont = parseInt(el.style.fontSize,10);
+//		el.style.fontSize = (curFont+step) + 'px';
+	}
+
 }]);
