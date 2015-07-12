eac2de168447211ec7189c37e01c81fd4c33fb21
Marco Ravazzini <marcoravazzini@marcos-MacBook-Pro.local>
2015-07-12 17:57:56 +0200

    add different type of list item in craigslist

--- Gender/public/controllers/craiglistPageCTRL.js
+++ Gender/public/controllers/craiglistPageCTRL.js
@@ -1,25 +1,25 @@
 
 myApp.controller('craigCTRL', function($scope,$http) {
     
-//   $scope.categories=[
-//       {name: 'sport', icon:'shopping_cart'},
-//       {name: 'lavoro' ,icon:'insert_emoticon'},
-//       {name: 'hobbie', icon:'work'},
-//       {name: 'sport'},
-//       {name: 'sport'},
-//       {name: 'sport'},
-//       {name: 'sport'},
-//       {name: 'sport'},
-//       {name: 'sport'}
-//   ]
+   $scope.categories=[
+       {name: 'sport', icon:'shopping_cart', type:1},
+       {name: 'lavoro' ,icon:'insert_emoticon',type:2},
+       {name: 'hobbie', icon:'work',type:1},
+       {name: 'sport',type:2},
+       {name: 'sport',type:1},
+       {name: 'sport',type:2},
+       {name: 'sport',type:1},
+       {name: 'sport',type:2},
+       {name: 'sport'}
+   ]
    
-    $http.get('/category').
-    success(function(data, status, headers, config) {
-      $scope.categories = data;
-    }).
-    error(function(data, status, headers, config) {
-        console.log(data);
-   });
+//    $http.get('/category').
+//    success(function(data, status, headers, config) {
+//      $scope.categories = data;
+//    }).
+//    error(function(data, status, headers, config) {
+//        console.log(data);
+//   });
     
     
 });
